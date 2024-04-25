b967dfc4a9b7ffff15e0fbbb35c87b6681b2ae18
IgorWinnik <poslugahey@gmail.com>
2024-04-25 21:01:19 +0300

    Create project

---
+++ ArtemVinnikLab/src/main/java/com/javaadvanced/ArtemVinnikLab/ArtemVinnikLabApplication.java
@@ -0,0 +1,13 @@
+package com.javaadvanced.ArtemVinnikLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtemVinnikLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtemVinnikLabApplication.class, args);
+	}
+
+}
